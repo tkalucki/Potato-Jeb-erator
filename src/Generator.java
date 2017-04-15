9388101a1cae8fe9957e9d717bb235dcc9a02160
jlalle19 <jax.9.allen@gmail.com>
2017-04-15 05:17:45 -0700

    Removal of Collision test
    Collision test removed due to .getX() and .getY() for type island only
    returning values of zero. ic.getX() should never return a zero but after
    testing it does so until the issues is resolved the code in question is
    removed from master.

--- src/Generator.java
+++ src/Generator.java
@@ -10,32 +10,9 @@ public class Generator {
 		IslandCircle islands[] = new IslandCircle[amount];
 		for(int i = 0; i<amount; i++){
 			IslandCircle ic = new IslandCircle(h, w, r, minr);
-			int j = i; 
-			if (i != 0) //if not the first iteration
-			{	
-				for(;0<=j;j--) //iterate for amount of initialized islands
-				{
-					int x_0 = islands[j].getX();
-					int y_0 = islands[j].getY();
-					int x_1 = ic.getX();
-					int y_1 = ic.getY();
-					int r_combined = islands[j].getR() + ic.getR();
-					int len = (int) Math.sqrt((x_0 - x_1)*(x_0 - x_1) + (y_0 - y_1)*(y_0 - y_1)); //Length between both origins
-					if(len <= r_combined)
-					{
-						j = -1; //Break the for loop if there is overlap
-					}
-					//else the island does not have a collision yet
-				}
-			}
-			if (j < 0)
-				i--; //if there was collision dont increment i and regenerate a new island
-			else
-			{
-				islands[i] = ic;
-				System.out.println("\nCircle Island#" + i);
-				ic.printInfo();
-			}
+			islands[i] = ic;
+			System.out.println("\nCircle Island#" + i);
+			ic.printInfo();
 		}
 		
 		IslandRectangle islands2[] = new IslandRectangle[amount];
