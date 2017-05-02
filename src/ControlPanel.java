de28961abcc21b2a3cc78f330a17ec5982ba1823
jlalle19 <jax.9.allen@gmail.com>
2017-05-01 19:48:15 -0700

    .

--- src/ControlPanel.java
+++ src/ControlPanel.java
@@ -15,6 +15,7 @@ public class ControlPanel extends JPanel
 	       panelNum = 2; //the number of panels is 2
 
 	       GenMapPanel genMapPanel = new GenMapPanel();
+	       IslMapPanel islMapPanel = new IslMapPanel();
 	       JSplitPane sp = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, genMapPanel, right);
 	       
       setPreferredSize(new Dimension(width,height));
--- src/DndMapCreator.java
+++ src/DndMapCreator.java
@@ -2,8 +2,8 @@ import javax.swing.*;
 
 public class DndMapCreator extends JApplet
 {
-	  private final int WIDTH = 802;
-	  private final int HEIGHT = 872;
+	  private final int WIDTH = 1600;
+	  private final int HEIGHT = 800;
  public void init()
   {
     ControlPanel controlPanel = new ControlPanel(WIDTH,HEIGHT);
